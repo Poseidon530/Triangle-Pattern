c23195158200ad69dde5fff486191326b97aa831
Saatvik Gaur <saatvik.gaur08@gmail.com>
2021-02-04 16:58:17 -0800

    Fixed Coloring

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-var draw = () => {
-	var c = document.getElementById('canvas');
-	var ctx = c.getContext('2d')
-	ctx.beginPath();
-	ctx.moveTo(500, 50);
-	ctx.lineTo(50, 500);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(50,500);
-    ctx.lineTo(550,500);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(550,500);
-    ctx.lineTo(500,50);
-	ctx.stroke();
-	ctx.fillStyle("rgb(255, 0, 0)")
-	ctx.fill();
-}
-draw();
-    
+function draw() {
+	var canvas = document.getElementById('canvas');
+	if (canvas.getContext) {
+	  var ctx = canvas.getContext('2d');
+  
+	  // var red = Math.floor(Math.random() * 255);
+	  // var green = Math.floor(Math.random() * 255);
+	  // var blue = Math.floor(Math.random() * 255);
+  
+	  ctx.beginPath();
+	  ctx.moveTo(100, 300);
+	  ctx.lineTo(300, 300);
+	  ctx.stroke()
+	  ctx.lineTo(200, 100);
+	  ctx.stroke()
+	  ctx.lineTo(100, 300);
+	  ctx.stroke()
+	  ctx.fillStyle = "blue";
+	// ctx.fillStyle = "rgb(" + red + "," + green + "," + blue + " )";  
+	  ctx.fill();
+	}
+  }
+  
+  draw()
+  
+  // setInterval(function(){ draw(); }, 500);
+  
